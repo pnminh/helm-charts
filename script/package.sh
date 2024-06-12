603ca4973c7beb1791a8ce8f8429fddcbf1b0f47
pnminh <pnminh232@gmail.com>
2024-06-12 17:39:01 -0500

    new script

--- script/package.sh
+++ script/package.sh
@@ -1,10 +1,11 @@
 # Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-for dir in $SCRIPT_DIR/../*/ ; do
+REPO_DIR="$SCRIPT_DIR/../helm-repo"
+for dir in $SCRIPT_DIR/../charts/*/ ; do
   echo $dir
   if [ -f "$dir/Chart.yaml" ]; then
     helm dependency update $dir
-    helm package "$dir"
+    helm package "$dir" --destination $REPO_DIR
   fi
 done
-helm repo index . --url https://pnminh.github.io/helm-charts
+helm repo index $REPO_DIR --url https://pnminh.github.io/helm-charts
